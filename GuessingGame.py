9e21a713e61372b888b0c95ddf93175844ba4694
kebron Gizachew <102880168+kebrongiz@users.noreply.github.com>
2023-04-20 02:39:56 +0300

    Add files via upload

---
+++ GuessingGame.py
@@ -0,0 +1,15 @@
+secret_word = "kebron"
+guess = " "
+guess_count = 0
+guess_limit = 3
+out_of_guess = False
+while guess != secret_word and not(out_of_guess) :
+    if guess_count < guess_limit:
+        guess = input("Enter your Guess:")
+        guess_count += 1
+    else:
+        out_of_guess = True
+if out_of_guess :
+    print("out of guesses, you lose!")
+else:
+    print("Hey You are a winner")
---
+++ README.md.py
@@ -0,0 +1,1 @@
+# play Guessing Game
